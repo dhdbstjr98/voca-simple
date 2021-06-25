173fe561af2410d1071d97ab202224f1a39b0bd8
오윤석 <dhdbstjr98@khu.ac.kr>
2021-06-25 16:25:48 +0900

    add auto meaning

--- src/component/Register.svelte
+++ src/component/Register.svelte
@@ -1,8 +1,12 @@
 <script>
+  import { words } from "../store";
+  import httpJsonp from "http-jsonp";
+  import { v4 } from "uuid";
+
   let show_register = false;
   let word = "";
   let meaning = "";
-  import { words } from "../store";
+  let ref_meaning = null;
 
   const register = () => {
     if (!word) {
@@ -18,6 +22,32 @@
     meaning = "";
     show_register = false;
   };
+
+  const getMeaning = async () =>
+    new Promise((resolve, reject) => {
+      const callback = `callback${v4().substring(0, 8)}`;
+      httpJsonp({
+        url: `https://suggest.dic.daum.net/language/v1/search.json?cate=lan&q=${word}`,
+        params: {
+          callback: callback
+        },
+        callbackProp: "callback",
+        callback: data => {
+          if (
+            !data ||
+            !data.items ||
+            !data.items.lan ||
+            data.items.lan.length === 0
+          ) {
+            M.toast({ html: "단어를 찾을 수 없습니다." });
+          }
+
+          [, word, meaning] = data.items.lan[0].item.split("|");
+
+          ref_meaning.dispatchEvent(new Event("focus"));
+        }
+      });
+    });
 </script>
 
 <style>
@@ -54,6 +84,19 @@
   .buttons {
     text-align: right;
   }
+
+  .word-wrapper {
+    padding-right: 6rem;
+    position: relative;
+  }
+
+  .word-wrapper button {
+    position: absolute;
+    right: 0;
+    bottom: 8px;
+    width: 5rem;
+    box-sizing: border-box;
+  }
 </style>
 
 {#if !show_register}
@@ -69,12 +112,24 @@
   </div>
 {:else}
   <div class="register z-depth-2">
-    <div class="input-field">
-      <input id="word" type="text" bind:value={word} />
-      <label for="word">단어</label>
+    <div class="word-wrapper">
+      <div class="input-field">
+        <input id="word" type="text" bind:value={word} />
+        <label for="word">단어</label>
+      </div>
+      <button
+        type="button"
+        class="btn waves-effect waves-light red accent-1"
+        on:click={getMeaning}>
+        자동
+      </button>
     </div>
     <div class="input-field">
-      <input id="meaning" type="text" bind:value={meaning} />
+      <input
+        id="meaning"
+        type="text"
+        bind:value={meaning}
+        bind:this={ref_meaning} />
       <label for="meaning">뜻</label>
     </div>
     <div class="buttons">
